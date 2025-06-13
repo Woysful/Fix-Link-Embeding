48e72b7b3408f61bbedc99b799d507924eb4e2b0
woysful <woysful@outlook.com>
2025-06-13 22:23:49 +0300

    Added context menu with a button to open replacements list

--- main.py
+++ main.py
@@ -7,53 +7,78 @@ sys.path.append(os.path.join(parent_folder_path, 'plugin'))
 
 from flowlauncher import FlowLauncher
 
-class NoLink(FlowLauncher):
+class ContextMenu:
+    def context_menu(self, data):
+        return [
+            {
+                "Title"     : "Replacements list",
+                "SubTitle"  : "Open config.JSON",
+                "IcoPath"   : "Images\\config.png",
+                "Score"     : 10000,
+                "JsonRPCAction": {
+                    "method"    : "open_replacements",
+                    "parameters": [],
+                    "dontHideAfterAction": False
+                }
+            }
+        ]
+
+    def open_replacements(self):
+        os.startfile(r".\plugin\replacements.json")
+
+class NoLink(FlowLauncher, ContextMenu):
     def query(self, query):
         return [
             {
-                "Title": "Copy the link first!",
-                "SubTitle": "it looks like you didn't copy the link",
-                "IcoPath": "Images\\Warning.png",
+                "Title"     : "Copy the link first!",
+                "SubTitle"  : "it looks like you didn't copy the link",
+                "IcoPath"   : "Images\\Warning.png"
             }
         ]
     
-class FixLink(FlowLauncher):
+    def context_menu(self, data):
+        return ContextMenu.context_menu(self, data)
+    
+class FixLink(FlowLauncher, ContextMenu):
     def query(self, query):
         return [
             {
                 "Title": "Fixed Short Link",
-                "SubTitle": config.domain_fix[0] + " | " + config.api_name,
-                "IcoPath": "Images\\FixedTiny.png",
-                "Score": 100000,
+                "SubTitle"  : config.domain_fix[0] + " | " + config.api_name,
+                "IcoPath"   : "Images\\FixedTiny.png",
+                "Score"     : 100000,
                 "JsonRPCAction": {
-                    "method": "run_fix",
+                    "method"    : "run_fix",
                     "parameters": ["short"],
                     "dontHideAfterAction": False
                 }
             },
             {
-                "Title": "Fixed Link",
-                "SubTitle": config.domain_fix[0],
-                "IcoPath": "Images\\Fixed.png",
-                "Score": 50000,
+                "Title"     : "Fixed Link",
+                "SubTitle"  : config.domain_fix[0],
+                "IcoPath"   : "Images\\Fixed.png",
+                "Score"     : 50000,
                 "JsonRPCAction": {
-                    "method": "run_fix",
+                    "method"    : "run_fix",
                     "parameters": ["long"],
                     "dontHideAfterAction": False
                 }
             },
             {
-                "Title": "Short Link",
-                "SubTitle": config.api_name,
-                "IcoPath": "Images\\TinyLink.png",
-                "Score": 0,
+                "Title"     : "Short Link",
+                "SubTitle"  : config.api_name,
+                "IcoPath"   : "Images\\TinyLink.png",
+                "Score"     : 0,
                 "JsonRPCAction": {
-                    "method": "run_fix",
+                    "method"    : "run_fix",
                     "parameters": ["tiny"],
                     "dontHideAfterAction": False
                 }
             }
         ]
+    
+    def context_menu(self, data):
+        return ContextMenu.context_menu(self, data)
 
     def run_fix(self, arg):
         from plugin.utils import run
